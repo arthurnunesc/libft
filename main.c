dbdea5f29cc2b2090d9973f68b5c0826721b4999
Arthur Nunes <arthurnunesc@proton.me>
2023-05-03 15:19:59 +0200

    Add isprint

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: anunes-c <anunesc-@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 12:38:36 by anunes-c          #+#    #+#             */
-/*   Updated: 2023/05/03 15:09:08 by anunes-c         ###   ########.fr       */
+/*   Updated: 2023/05/03 15:12:15 by anunes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,13 @@ int	main(void)
 	int	var_digit;
 	int	var_ascii;
 	int	var_negative;
+	int	var_unprint;
 
 	var_alpha = 45;
 	var_digit = 105;
 	var_ascii = 127;
 	var_negative = -1;
+	var_unprint = 12;
 	printf("isalpha(%d): %d\n", var_alpha, isalpha(var_alpha));
 	printf("ft_isalpha(%d): %d\n", var_alpha, ft_isalpha(var_alpha));
 	printf("isalpha(%d): %d\n", var_digit, isalpha(var_digit));
@@ -52,5 +54,16 @@ int	main(void)
 	printf("ft_isalnum(%d): %d\n", var_ascii, ft_isalnum(var_ascii));
 	printf("isalnum(%d): %d\n", var_negative, isalnum(var_negative));
 	printf("ft_isalnum(%d): %d\n", var_negative, ft_isalnum(var_negative));
+	printf("\n");
+	printf("isprint(%d): %d\n", var_alpha, isprint(var_alpha));
+	printf("ft_isprint(%d): %d\n", var_alpha, ft_isprint(var_alpha));
+	printf("isprint(%d): %d\n", var_digit, isprint(var_digit));
+	printf("ft_isprint(%d): %d\n", var_digit, ft_isprint(var_digit));
+	printf("isprint(%d): %d\n", var_ascii, isprint(var_ascii));
+	printf("ft_isprint(%d): %d\n", var_ascii, ft_isprint(var_ascii));
+	printf("isprint(%d): %d\n", var_negative, isprint(var_negative));
+	printf("ft_isprint(%d): %d\n", var_negative, ft_isprint(var_negative));
+	printf("isprint(%d): %d\n", var_unprint, isprint(var_unprint));
+	printf("ft_isprint(%d): %d\n", var_unprint, ft_isprint(var_unprint));
 	printf("\n");
 }
