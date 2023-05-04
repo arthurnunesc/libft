040ceabd5ed2907d89112dfd4f273835674a9ca1
Arthur Nunes <arthurnunesc@proton.me>
2023-05-04 12:26:51 +0200

    Fix norminette and delete main.c

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: anunes-c <anunesc-@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 17:41:25 by anunes-c          #+#    #+#             */
-/*   Updated: 2023/05/03 18:32:02 by anunes-c         ###   ########.fr       */
+/*   Updated: 2023/05/04 12:11:51 by anunes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
 	unsigned char	*uc;
 
 	uc = (unsigned char *)s;
