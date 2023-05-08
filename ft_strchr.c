2384e92f16be9bed2f5ab406442dc6ba36b4cb8b
Arthur Nunes Correa <anunes-c@car3s4.42barcelona.com>
2023-05-08 14:12:30 +0200

    Add strchr

---
+++ ft_strchr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anunes-c <anunesc-@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 13:35:30 by anunes-c          #+#    #+#             */
+/*   Updated: 2023/05/08 14:12:15 by anunes-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned char	uc;
+	size_t			i;
+
+	i = 0;
+	uc = (unsigned char)c;
+	while (s[i])
+	{
+		if (s[i] == uc)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == uc)
+		return ((char *)&s[i]);
+	return (NULL);
+}
