b85348b3e37aab5cde29d4844d60edade74f9222
Arthur Nunes Correa <anunes-c@car3s4.42barcelona.com>
2023-05-08 15:22:22 +0200

    Delete swap file, correct strrchr and start strnstr correction

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: anunes-c <anunes-c@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 13:48:51 by anunes-c          #+#    #+#             */
-/*   Updated: 2023/05/05 18:44:04 by anunes-c         ###   ########.fr       */
+/*   Updated: 2023/05/08 14:42:01 by anunes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,27 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	char	*big_big;
+	char	*result;
 
 	i = 0;
 	j = 0;
-	big_big = (char *)big;
-	if (little[0] == '\0' || len == 0)
-		return (big_big);
-	while (big[i])
+	result = NULL;
+	if (little == NULL || little[0] == '\0')
+		return ((char *)big);
+	while (big[i] && i < len)
 	{
 		if (big[i] == little[j])
 		{
-			while (little[j] && big[i + j] == little[j])
+			while (big[i + j] == little[j] && i + j < len)
 				j++;
 			if (little[j] == '\0')
-				return (&big_big[i]);
+			{
+				result = ((char *)&big[i]);
+				break ;
+			}
 			j = 0;
 		}
 		i++;
 	}
-	return (0);
+	return (result);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,26 +6,26 @@
 /*   By: anunes-c <anunesc-@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 13:35:30 by anunes-c          #+#    #+#             */
-/*   Updated: 2023/05/08 14:15:18 by anunes-c         ###   ########.fr       */
+/*   Updated: 2023/05/08 15:21:38 by anunes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
 	unsigned char	uc;
-	size_t			i;
+	int				i;
 	char			*result;
 
 	i = 0;
+	result = 0;
 	uc = (unsigned char)c;
 	while (s[i])
 	{
 		if (s[i] == uc)
 			result = (char *)&s[i];
-		i++;
+		s++;
 	}
 	if (s[i] == uc)
 		result = (char *)&s[i];
