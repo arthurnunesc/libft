1434841ec19a54260fe3d30d41e70a072fa4bee9
Arthur Nunes Correa <anunes-c@car3s5.42barcelona.com>
2023-05-09 16:47:08 +0200

    Add strtrim file

---
+++ ft_strtrim.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anunes-c <anunesc-@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 16:41:40 by anunes-c          #+#    #+#             */
+/*   Updated: 2023/05/09 16:45:47 by anunes-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char			*trimmed;
+	unsigned int	len;
+	unsigned int	i;
+
+	i = 0;
+	return (trimmed);
+}
