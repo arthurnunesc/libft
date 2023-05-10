fc68269ee140cf79377a6554d834dc88e7eaa9a7
Arthur Nunes <arthurnunesc@proton.me>
2023-05-10 23:11:10 +0200

    Add strstr and strrstr prototyping to strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,9 +6,19 @@
 /*   By: anunes-c <anunesc-@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 16:41:40 by anunes-c          #+#    #+#             */
-/*   Updated: 2023/05/09 16:45:47 by anunes-c         ###   ########.fr       */
+/*   Updated: 2023/05/10 23:10:45 by anunes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+static char	*strstr(const char *big, const char	*little)
+{
+	return ((char *)big);
+}
+
+static char	*strrstr(const char	*str, const char *pat)
+{
+	return ((char *)str);
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
