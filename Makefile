45b38c02e7f5d98b939f6704a17438fe6b8898f7
Arthur Nunes Correa <anunes-c@cbr4s4.42barcelona.com>
2023-05-11 15:56:20 +0200

    Go back to using old Makefile to test strtrim

--- Makefile
+++ Makefile
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    Makefile                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: anunes-c <anunes-c@student.42barcel>       +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2023/05/04 13:09:12 by anunes-c          #+#    #+#              #
-#    Updated: 2023/05/04 13:09:14 by anunes-c         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 NAME = libft.a
 
 SRCS = $(wildcard *.c)
@@ -23,10 +11,10 @@ CFLAGS = -Wall -Wextra -Werror
 all: $(NAME)
 
 $(NAME): $(OBJS)
-	@ar rcs $(NAME) $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -o $(NAME)
 
 %.o: %.c
-	@$(CC) $(CFLAGS) -c $< -o $@
+	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
 	rm -f $(OBJS)
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,27 +5,81 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: anunes-c <anunesc-@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/09 16:41:40 by anunes-c          #+#    #+#             */
-/*   Updated: 2023/05/10 23:10:45 by anunes-c         ###   ########.fr       */
+/*   Created: 2023/05/11 13:43:26 by anunes-c          #+#    #+#             */
+/*   Updated: 2023/05/11 15:53:26 by anunes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static char	*strstr(const char *big, const char	*little)
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include "libft.h"
+
+int	ft_findfirstcharnotinset(char const *s1, char const *set)
 {
-	return ((char *)big);
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	found;
+
+	i = 0;
+	j = 0;
+	found = 0;
+	while (s1[i])
+	{
+		while (set[j])
+		{
+			if (ft_strchr(s1 + i, set[j]))
+				found = 1;
+			j++;
+		}
+		i++;
+		if (found == 0)
+			break ;
+		found = 0;
+		j = 0;
+	}
+	return (i);
 }
 
-static char	*strrstr(const char	*str, const char *pat)
+int	ft_findlastcharnotinset(char const *s1, char const *set)
 {
-	return ((char *)str);
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	found;
+	unsigned int	s1_len;
+
+	j = 0;
+	found = 0;
+	s1_len = ft_strlen(s1);
+	i = s1_len - 1;
+	while (s1[i])
+	{
+		while (set[j])
+		{
+			if (ft_strrchr(s1 + i, set[j]) != NULL)
+				found = 1;
+			j++;
+		}
+		i--;
+		if (found == 0)
+			break ;
+		found = 0;
+		j = 0;
+	}
+	return (i);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char			*trimmed;
-	unsigned int	len;
-	unsigned int	i;
+	unsigned int	start;
+	// unsigned int	len;
 
-	i = 0;
+	trimmed = 0;
+	start = ft_findfirstcharnotinset(s1, set);
+	// len = start - ft_findlastcharnotinset(s1, set);
+	// trimmed = ft_substr(s1, start, len);
+	printf("%d\n", start);
+	// printf("%d\n", len);
 	return (trimmed);
 }
