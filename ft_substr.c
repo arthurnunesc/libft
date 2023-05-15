314781b46f1d2447c8cc76ef3100042b1680c819
Arthur Nunes Correa <anunes-c@car6s1.42barcelona.com>
2023-05-15 19:36:15 +0200

    Correct substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: anunes-c <anunesc-@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 14:34:47 by anunes-c          #+#    #+#             */
-/*   Updated: 2023/05/09 15:28:26 by anunes-c         ###   ########.fr       */
+/*   Updated: 2023/05/15 19:30:15 by anunes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*substr;
 	size_t	i;
 	size_t	s_len;
+	size_t	malloc_size;
 
 	substr = 0;
 	i = 0;
 	s_len = ft_strlen(s);
-	if (start >= s_len || len > s_len)
-		return (0);
-	substr = (char *)malloc(sizeof(char) * len + 1);
+	malloc_size = len + 1;
+	if (malloc_size > s_len - start + 1)
+		malloc_size = s_len - start + 1;
+	if (start >= s_len)
+		malloc_size = 1;
+	substr = (char *)ft_calloc(malloc_size, sizeof(char));
 	if (substr == NULL)
 		return (0);
 	while (i < len && start + i < s_len)
