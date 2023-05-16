51ed68e57125395f2cf06f1c1419055e5849dd71
Arthur Nunes Correa <anunes-c@car8s4.42barcelona.com>
2023-05-16 15:06:11 +0200

    Correct itoa norminette

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,28 +6,16 @@
 /*   By: anunes-c <anunesc-@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 18:41:32 by anunes-c          #+#    #+#             */
-/*   Updated: 2023/05/15 18:52:00 by anunes-c         ###   ########.fr       */
+/*   Updated: 2023/05/16 14:59:03 by anunes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+static char	*ft_createandfillstr(int n, int size)
 {
 	char	*dst;
-	int		size;
-	int		size_aux;
 
-	size_aux = n;
-	size = 0;
-	if (n == -2147483648)
-		return (ft_strdup("-2147483648"));
-	if (n == 0)
-		return (ft_strdup("0"));
-	if (n < 0)
-		size++;
-	while (++size && (size_aux > 9 || size_aux < -9))
-		size_aux = size_aux / 10 ;
 	dst = (char *)ft_calloc((size + 1), sizeof(char));
 	if (dst == NULL)
 		return (0);
@@ -42,4 +30,22 @@ char	*ft_itoa(int n)
 		n = n / 10;
 	}
 	return (dst);
+}
+
+char	*ft_itoa(int n)
+{
+	int		size;
+	int		size_aux;
+
+	size_aux = n;
+	size = 0;
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	if (n == 0)
+		return (ft_strdup("0"));
+	if (n < 0)
+		size++;
+	while (++size && (size_aux > 9 || size_aux < -9))
+		size_aux = size_aux / 10 ;
+	return (ft_createandfillstr(n, size));
 }
