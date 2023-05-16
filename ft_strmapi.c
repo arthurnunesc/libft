a5f2e679cfd003842a1a3d3e88dbcd102dc190af
Arthur Nunes Correa <anunes-c@car8s4.42barcelona.com>
2023-05-16 15:55:12 +0200

    Add strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anunes-c <anunesc-@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 15:10:04 by anunes-c          #+#    #+#             */
+/*   Updated: 2023/05/16 15:54:51 by anunes-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	s_len;
+	unsigned int	malloc_size;
+	char			*dst;
+	unsigned int	i;
+
+	i = 0;
+	s_len = ft_strlen(s);
+	malloc_size = s_len + 1;
+	dst = ft_calloc(malloc_size, sizeof(char));
+	if (dst == NULL)
+		return (0);
+	while (i < s_len)
+	{
+		dst[i] = f(i, s[i]);
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
