30c75ce1f8af26f5ca0a82271b244bc0dced2197
Arthur Nunes Correa <anunes-c@car6s4.42barcelona.com>
2023-05-17 12:46:52 +0200

    Add striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: anunes-c <anunesc-@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 15:10:04 by anunes-c          #+#    #+#             */
-/*   Updated: 2023/05/16 16:03:16 by anunes-c         ###   ########.fr       */
+/*   Updated: 2023/05/17 12:45:24 by anunes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	unsigned int	i;
 
 	i = 0;
-	while (s[i])
+	while (*s)
 	{
 		f(i, s);
 		i++;
+		s++;
 	}
 }
