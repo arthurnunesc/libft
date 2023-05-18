f27174be0b6d40fe90a1547b1b123f7207c77d0e
Arthur Nunes <arthurnunesc@proton.me>
2023-05-18 12:01:35 +0200

    Add putendl_fd and correct norminette of putchar_fd

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -1,18 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anunes-c <anunesc-@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/18 11:54:58 by anunes-c          #+#    #+#             */
-/*   Updated: 2023/05/18 12:01:11 by anunes-c         ###   ########.fr       */
+/*   Created: 2023/05/18 11:59:29 by anunes-c          #+#    #+#             */
+/*   Updated: 2023/05/18 12:00:22 by anunes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include "libft.h"
 
-void	ft_putchar_fd(char c, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
-	write(fd, &c, 1);
+	ft_putstr_fd(s, fd);
+	write(fd, "\n", 1);
 }
