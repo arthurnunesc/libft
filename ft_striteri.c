c5d5eb89b1f6f13f54036c3353d086441e22e688
Arthur Nunes <arthurnunesc@proton.me>
2023-05-29 14:40:45 +0200

    Delete unnecessary libft.h

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,11 +6,9 @@
 /*   By: anunes-c <anunesc-@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 15:10:04 by anunes-c          #+#    #+#             */
-/*   Updated: 2023/05/18 00:53:02 by anunes-c         ###   ########.fr       */
+/*   Updated: 2023/05/29 14:10:49 by anunes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
